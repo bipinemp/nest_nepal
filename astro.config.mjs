c38ff807cb13377b43a40564237188cb3e3ecac3
Bipin Bhandari <bhandaribipin3.39@gmail.com>
2023-01-04 19:22:20 +0545

    added astro.confit.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: netlify()
 });
